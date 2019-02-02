181a426ec02741c5be943eb36c9a418f79b4bfe6
lovro <biljeskovic.lovro@gmail.com>
2019-02-02 18:13:11 +0100

    updating existing solution

--- simple_db.rb
+++ simple_db.rb
@@ -1,21 +1,49 @@
 class SimpleDb
-  def initialize
-    @movies = {}
-  end
 
-  def write_to_db(key, value)
-    @movies.store(key, value)
-  end
+def initialize
+  @movies = {}
+  @file = "test.db"
+end
 
-  def read_from_db(movies)
-    @movies.each_pair do |key, value|
-      puts "key: #{key} value: #{value}"
+def write_to_file(key, value)
+  File.open(@file, "a") { |f| f << "#{key};#{value}\n" }
+end
+
+def populate(file)
+  db = File.open(@file, "r")
+  key = ""
+  was_key_found = false
+  while true do
+    character = db.read(1)
+    unless character
+      break
+    end
+    if (character == "\n")
+      was_key_found = false
+    elsif (character != ";" and not was_key_found)
+      key += character
+    elsif (character == ";")
+      @movies[key] = db.tell()
+      was_key_found = true
+      key = ""
     end
   end
 end
 
-c = SimpleDb.new
-c.write_to_db("1", "memento")
-c.write_to_db("2", "interstellar")
-c.write_to_db("3", "prestige")
-c.read_from_db(@movies)
+def print(file)
+  db = File.open(@file, "r")
+  @movies.each_pair do |key, offset|
+    db.seek(offset)
+    value = db.readline().rstrip
+    puts "#{key}: #{value}\n"
+  end
+end
+
+
+end
+
+test_db = SimpleDb.new
+test_db.write_to_file("1", "memento")
+test_db.write_to_file("2", "inception")
+test_db.populate(@file)
+test_db.print(@file)
